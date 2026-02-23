5770d7dcb9d4dc2e9cf1a4cd2bd8fb2c3e1f4509
IBrop <step.shch.es07@gmail.com>
2026-02-23 22:24:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
+    print(f"Bot is online as {bot.user}")
+
     activity = discord.Game(name="Politerax | Minecraft 🌍")
     await bot.change_presence(
         status=discord.Status.online,
         activity=activity
     )
-    print(f"Bot is online as {bot.user}")
-
-@bot.event
-async def on_ready():
-    print(f"Bot is online as {bot.user}")
 
 @bot.command()
 async def ping(ctx):
